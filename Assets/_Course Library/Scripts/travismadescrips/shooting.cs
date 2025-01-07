22f98907f658a5313e8586e46ce8f613dd8567da
Travis m <murdocktravis03@gmail.com>
2025-01-07 10:22:39 -0800

    starting gun working

---
+++ Assets/_Course Library/Scripts/travismadescrips/shooting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject bulletSpawn;
+    public GameObject bullet;
+    public GameObject casing;
+    public float bulletSpeed;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
