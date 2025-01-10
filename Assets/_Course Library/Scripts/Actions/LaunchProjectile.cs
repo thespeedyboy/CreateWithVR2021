7a034c7941960089daaba938ede7e53d78d1c588
Travis m <murdocktravis03@gmail.com>
2025-01-10 10:12:55 -0800

    stuff

--- Assets/_Course Library/Scripts/Actions/LaunchProjectile.cs
+++ Assets/_Course Library/Scripts/Actions/LaunchProjectile.cs
@@ -8,24 +8,46 @@ public class LaunchProjectile : MonoBehaviour
 {
     [Tooltip("The projectile that's created")]
     public GameObject projectilePrefab = null;
-
     [Tooltip("The point that the project is created")]
     public Transform startPoint = null;
 
     [Tooltip("The speed at which the projectile is launched")]
     public float launchSpeed = 6.0f;
-
+    public bool canShoot = false;
+    private AudioSource audiosource;
+    public AudioClip dryfire;
+    public AudioClip fire;
+    public void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
     public void Fire()
     {
-        GameObject newObject = Instantiate(projectilePrefab, startPoint.position, startPoint.rotation);
+        if (canShoot == true)
+        {
+            GameObject newObject = Instantiate(projectilePrefab, startPoint.position, startPoint.rotation);
 
-        if (newObject.TryGetComponent(out Rigidbody rigidBody))
-            ApplyForce(rigidBody);
+            if (newObject.TryGetComponent(out Rigidbody rigidBody))
+                ApplyForce(rigidBody);
+            audiosource.PlayOneShot(fire);
+        }
+        if (canShoot == false)
+        {
+            audiosource.PlayOneShot(dryfire);
+        }
     }
 
     private void ApplyForce(Rigidbody rigidBody)
     {
         Vector3 force = startPoint.forward * launchSpeed;
         rigidBody.AddForce(force);
+    }
+    public void EnableShooting()
+    {
+        canShoot = true;
+    }
+    public void DisableShooting()
+    {
+        canShoot = false;
     }
 }
